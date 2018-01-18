3795a62b07b8ceb6b0f1990f89a512e0813d6307
Chris Hettrick <git@structfoo.com>
2018-01-18 15:56:42 -0800

    Implemented argument parsing into a struct.
    The line returned from readline() must be parsed and tokenized.
    This has to be done twice, since argc needs to be known to
    malloc argv in the next step.
    
    The first go through the line is counting argc with a state
    machine (if in a whitespace delimiter or out).
    
    The second go through the line is with strsep() (like strtok())
    and tokenizing on the delimiters (again), and building up argv.
    
    argv then needs to be malloc'd, since it is on the stack.
    
    A struct args holds the malloc'd argv, argc, the command,
    and whether it runs in the foreground or the background.
    
    Could decompose this function into smaller ones.
    Also, can get rid of the argv on the stack, and only use the
    malloc'd argv. Cleanup of debugging code is needed.

--- sh.c
+++ sh.c
@@ -1,48 +1,95 @@
+/* sh.c
+ * SSI: Simple Shell Interpreter
+ *
+ * CSC360 - 2018 Spring
+ * Dr. Jianping Pan
+ *
+ * Christopher Hettrick
+ */
+
 #include <err.h>		/* err(3) */
 #include <limits.h>		/* PATH_MAX */
 #include <stdio.h>		/* printf(3), snprintf(3), readline(3) */
 #include <stddef.h>		/* size_t */
-#include <stdlib.h>		/* exit(3), free(3), getenv(3) */
+#include <stdlib.h>		/* exit(3), free(3), getenv(3), calloc(3) */
 #include <string.h>		/* strdup(3) */
 #include <strings.h>		/* strcasecmp(3) */
-#include <unistd.h>		/* getcwd(3) */
+#include <unistd.h>		/* getcwd(3), fork(2) */
 
 #include <readline/readline.h>	/* readline(3) */
 
 #define PROMPT_SIZE	(5 + PATH_MAX + 3 + 1)
 
-struct proc {
-	struct	proc *next;
-	int	bar;
+enum proc_state {
+	STATE_FG,
+	STATE_BG
 };
 
-static void		cwd_prompt(char *, int);
-static void		usage(void);
+struct args {
+	char	 *file;
+	char	**argv;
+	int	  argc;
+	enum	  proc_state ps;
+};
 
+struct proc {
+	struct	  proc *next;
+	pid_t	  pid;
+	struct	  args *a;
+};
+
+static void		 cwd_prompt(char *, size_t);
+static struct args	*parse_args(char *);
+static void		 usage(void) __attribute__ ((__noreturn__));
+
+/*
+ * SSI: Simple Shell Interpreter
+ *
+ * Very basic Bourne Shell functionality.
+ *
+ * Caveats: Quoted arguments are not supported.
+ *          Backslash escapes are not supported.
+ */
 int
 main(int argc, char *argv[])
 {
 	char		*line;
 	char		 prompt[PROMPT_SIZE];
 	const char	*home_dir;
+	struct args	*args;
 
 	if (argc > 1) {
 		usage();
 	}
 	argc--;
-	argv++;			/* To satiate the compiler. */
+	argv++;			/* XXX To satiate the compiler. */
 
 #if 0
 	printf("\n%s\n", getenv("PATH"));
 #endif
 
 	if ((home_dir = getenv("HOME")) == NULL) {
-		fprintf(stderr, "HOME enviroment variable not set");
+		fprintf(stderr, "HOME environment variable not set");
 		err(1, "getenv");
 	}
 
 	cwd_prompt(prompt, PROMPT_SIZE);
 	while ((line = readline(prompt)) != NULL) {
+		printf("Line length: %lu\n", strlen(line));	/* XXX */
+		if ((args = parse_args(line)) == NULL) {
+			free(line);
+			continue;		/* Skip blank lines. */
+		}
+
+/* XXX */
+int i;
+	for (i = 0; args->argv[i] != '\0'; i++) {
+		printf("argv[%d]: %s\n", i, args->argv[i]);
+	}
+printf("Command name: %s\n", args->file);
+/* XXX */
+
+/* XXX Get cmd from struct */
 		if (!strcasecmp(line, "exit")) {
 			free(line);
 			return 0;
@@ -57,7 +104,7 @@ main(int argc, char *argv[])
 }
 
 static void
-cwd_prompt(char *prompt, int promptsize)
+cwd_prompt(char *prompt, size_t promptsize)
 {
 	char		*buf;
 	char		*p;
@@ -72,28 +119,127 @@ cwd_prompt(char *prompt, int promptsize)
 	}
 
 	ret = snprintf(prompt, promptsize, "SSI: %s > ", buf);
-	if (ret == -1 || ret >= promptsize) {
+	if (ret == -1 || ret >= (int)promptsize) {
 		err(1, "snprintf");
 	}
 	free(buf);
 }
 
+/* Does not work with quotes, yet. */
+struct args *
+parse_args(char *line)
+{
+	enum state {
+			 DELIM,
+			 OTHER
+	} state;
+
+	const char	*delim = " \t";	/* Delimiters between arguments. */
+	int		 argc;		/* Count of arguments in string. */
+	char		*c;		/* Current token in string. */
+
+	char		 *p;
+	char		**ap;
+	char		**newargv;
+	struct args	 *args;
+	int		  i;			/* XXX */
+
+	if (strlen(line) == 0) {	/* Only work on strings with tokens. */
+		return NULL;
+	}
+
+	/* Choose initial state as DELIM if first char is in delim. */
+	(strspn(line, delim) > 0) ? (state = DELIM) : (state = OTHER);
+
+	argc = 0;
+	for (c = line; *c != '\0'; c++) {
+		switch (state) {
+		case DELIM:
+			c += strspn(c, delim) - 1;
+			state = OTHER;
+			break;
+		case OTHER:
+			argc++;
+			c += strcspn(c, delim) - 1;
+			state = DELIM;
+			break;
+		/* No default: since state is an enum; no other cases. */
+		}
+	}
+
+	printf("argc: %d\n", argc);		/* XXX */
+
+	char		 *argv[argc + 1];	/* XXX - Here bc needs argc. */
+
+	/* Need a copy of the line, since it will be clobbered. */
+	if ((p = strdup(line)) == NULL) {
+		err(1, "strdup");
+	}
+
+	/* Now build argv. */
+	for (ap = argv; ap < &argv[argc] &&
+	    (*ap = strsep(&p, delim)) != NULL;) {
+		if (**ap != '\0') {
+			ap++;
+		}
+	}
+	*ap = NULL;
+
+	argv[argc] = NULL;
+
+
+	/* Need newargv on the heap, not on the stack like argv. */
+	if ((newargv = calloc((size_t)argc + 1, sizeof(*newargv))) == NULL) {
+		err(1, "calloc");
+	}
+
+	for (i = 0; i < argc; i++) {
+		if ((newargv[i] = strdup(argv[i])) == NULL) {
+			err(1, "strdup");
+		}
+	}
+	newargv[argc] = NULL;
+
+	/* Now make the struct to return with argc and newargv. */
+	if ((args = calloc(1, sizeof(args))) == NULL) {
+		err(1, "calloc");
+	}
+
+	if (!strcmp(newargv[0], "bg")) {
+		args->ps = STATE_BG;
+		args->file = newargv[1];
+	} else {
+		args->ps = STATE_FG;
+		args->file = newargv[0];
+	}
+	args->argv = newargv;
+	args->argc = argc;
+
+
+	return args;
+}
+
+
+
+
+
+
 #if 0
-	args usage();
+done	args usage();
 
-	char * to mallocd array of paths = parse_path($PATH);
-	if char * is NULL then
-		use _PATH_DEFPATH from paths.h
+n/a	char * to mallocd array of paths = parse_path($PATH);
+n/a	if char * is NULL then
+n/a		use _PATH_DEFPATH from paths.h
 
-	save $HOME directory for use with ~
+done	save $HOME directory for use with ~
 
 done	make up initial custom cwd prompt with function cwd_prompt(pre, post)
 
-done	while get a line from readline using custom cwd prompt {
+done	while get a line from readline using custom cwd prompt
 XXX actually have a flag set in proc struct, not a global
-		bg_flag = 0;	/* Child process runs in fg by default */
 
 		struct proc p = parse_line(line); /* Ret alloc struct or NULL */
+			proc_state = fg;	/* Child in fg by default */
 		if (p == NULL)
 			error message;
 
@@ -124,7 +270,7 @@ XXX actually have a flag set in proc struct, not a global
 		else
 			if cmd is 'bg'
 				shift first arg of arg string to be struct cmd
-				set bg_flag = 1;
+				set proc_state to bg
 				fork and exec, but dont wait.
 				detach child from stdin, stdout, stderr
 				add to processes linked list
@@ -135,12 +281,6 @@ XXX actually have a flag set in proc struct, not a global
 		if child exits then
 			print "pid: cmd options has terminated."
 		update cwd (because could be different now)
-	}
-#endif
-
-#if 0
-struct myproc parse_line(char *line);
-char *	parse_path(PATH);
 #endif
 
 static void
